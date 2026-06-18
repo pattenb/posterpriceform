d1de0e03fc298a0ac0d2ab175dd9d88b4f639fd2
Pat <borrapat@gmail.com>
2026-06-18 09:14:22 +0200

    Add GitHub Pages automated workflow
    Add GitHub Pages automated workflow

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import {defineConfig} from 'vite';
 export default defineConfig(() => {
   return {
     plugins: [react(), tailwindcss()],
+    base: '/posterpriceform/',
     resolve: {
       alias: {
         '@': path.resolve(import.meta.dirname || '.'),
